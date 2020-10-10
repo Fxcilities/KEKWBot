6774290b4acd50dca1d96c57a3dd637d50f64371
Fxcilities <48619853+Fxcilities@users.noreply.github.com>
2020-10-09 21:03:34 -0400

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import io
+import os
+from dotenv import load_dotenv()
+
+load_dotenv()
+
+token = os.environ["token"]
+prefix = os.environ["prefix"]
+
+bot = commands.Bot(command_prefix=prefix)
+
+
+@bot.command()
+@commands.is_owner()
+async def start(ctx, amount: int = 50):
+  em1 = discord.Embed(title="Starting...", color=discord.Color.blue(), description=f"I will start uploading **{amount}** kekw emojis!")
+  em2 = discord.Embed(title="Finished", color=discord.Color.green())
+  msg = await ctx.send(embed=em1)
+  cur = int(1)
+  while cur <= amount:
+    with open("kekw.png", "rb") as f:
+      res = f.read()
+      await ctx.guild.create_custom_emoji(name=f"kekw{cur}", image=res)
+      print("Uploaded: kekw{} to {}".format(cur, ctx.guild.name))
+      cur += 1
+  await msg.edit(embed=em2)
+
+@bot.event
+async def on_ready():
+  print(f"Bot ready, logged in as {bot.user} ({bot.user.id})")
+
+bot.run(token)
